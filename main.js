fe492600d2939c6e1e588fe335bf0b7398cedc24
Smith <andras@sexybuddha.hu>
2017-07-03 16:29:34 +0200

    preview + tweaks
     * added adapter js
     * added video element
     * capture entire screen button
     * stop capture button

---
+++ main.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const sourcesDiv = document.getElementById('sources');
+const sourceTypes = ['screen'];//, 'window', 'desktop'];
+const thumbSize = {width: 300, height: 300};
+
+window.desktopCapturer.getSources({types: sourceTypes, thumbnailSize: thumbSize}, (err, sources) => {
+    if (err) {
+        console.error('desktopCapturer.getSources error', err);
+        throw err;
+    }
+
+    console.info('desktopCapturer sources', JSON.stringify(sources, null, 2));
+
+    sourcesDiv.innerHTML = '';
+    sources.forEach(dcSource => {
+        let item = document.createElement('div');
+
+        let btn = document.createElement('button');
+        btn.textContent = `${dcSource.name} (${dcSource.id})`;
+        btn.onclick = function () {
+            setVideo({
+                audio: false,
+                video: {
+                    mandatory: {
+                        chromeMediaSource: 'desktop',
+                        chromeMediaSourceId: dcSource.id
+                    }
+                }
+            });
+        };
+
+        let img = new Image();
+        img.src = dcSource.thumbnail.toDataURL();
+
+        item.appendChild(img);
+        item.appendChild(document.createElement('br'));
+        item.appendChild(btn);
+        sourcesDiv.appendChild(item);
+    });
+});
+
+const video = document.getElementById('video');
+function setVideo (mediaConstraints) {
+    navigator.mediaDevices.getUserMedia(mediaConstraints)
+        .then(stream => {
+            stopVideo();
+            video.srcObject = stream;
+        })
+        .catch(err => {
+            console.error('getUserMedia error', err);
+            throw err;
+        });
+}
+
+function stopVideo () {
+    if (video.srcObject) {
+        video.srcObject.getTracks().forEach(t => {
+            t.stop();
+        });
+
+        video.srcObject = null;
+    }
+}
+
+function captureScreen () {
+    setVideo({
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'screen'
+            }
+        }
+    });
+}
