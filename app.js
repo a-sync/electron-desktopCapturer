b50445de427acaf3123415678a12b5a840803377
smith <andras@sexybuddha.hu>
2017-12-24 01:02:13 +0100

    show errors in alerts
     * stop current stream before trying to load a new one

--- app.js
+++ app.js
@@ -6,50 +6,50 @@ function getSources(type) { // eslint-disable-line no-unused-vars
     window.desktopCapturer.getSources({types: [type], thumbnailSize: thumbSize}, (err, sources) => {
         if (err) {
             console.error('desktopCapturer.getSources error', err);
-            throw err;
-        }
+            alert(JSON.stringify(err, null, 2)); // eslint-disable-line no-alert
+        } else {
+            console.info('desktopCapturer sources', JSON.stringify(sources, null, 2));
 
-        console.info('desktopCapturer sources', JSON.stringify(sources, null, 2));
+            sourcesDiv.innerHTML = '';
+            sources.forEach(dcSource => {
+                const item = document.createElement('div');
 
-        sourcesDiv.innerHTML = '';
-        sources.forEach(dcSource => {
-            const item = document.createElement('div');
-
-            const btn = document.createElement('button');
-            btn.textContent = `${dcSource.name} (${dcSource.id})`;
-            btn.onclick = function () {
-                setVideo({
-                    audio: false,
-                    video: {
-                        mandatory: {
-                            chromeMediaSource: 'desktop',
-                            chromeMediaSourceId: dcSource.id
+                const btn = document.createElement('button');
+                btn.textContent = `${dcSource.name} (${dcSource.id})`;
+                btn.onclick = function () {
+                    setVideo({
+                        audio: false,
+                        video: {
+                            mandatory: {
+                                chromeMediaSource: 'desktop',
+                                chromeMediaSourceId: dcSource.id
+                            }
                         }
-                    }
-                });
-            };
+                    });
+                };
 
-            const img = new Image();
-            img.src = dcSource.thumbnail.toDataURL();
+                const img = new Image();
+                img.src = dcSource.thumbnail.toDataURL();
 
-            item.appendChild(btn);
-            item.appendChild(document.createElement('br'));
-            item.appendChild(img);
-            sourcesDiv.appendChild(item);
-        });
+                item.appendChild(btn);
+                item.appendChild(document.createElement('br'));
+                item.appendChild(img);
+                sourcesDiv.appendChild(item);
+            });
+        }
     });
 }
 
 const video = document.getElementById('video');
 function setVideo(mediaConstraints) {
+    stopVideo();
     navigator.mediaDevices.getUserMedia(mediaConstraints)
         .then(stream => {
-            stopVideo();
             video.srcObject = stream;
         })
         .catch(err => {
             console.error('getUserMedia error', err);
-            throw err;
+            alert(JSON.stringify(err, null, 2)); // eslint-disable-line no-alert
         });
 }
 
